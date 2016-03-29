75b1b0d508877f636069485e4da55ae6de8fb322
Renaud <contact@rubiano.fr>
2016-03-30 00:16:05 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+  name = 'pydevicemanager',
+  packages = ['pydevicemanager'], 
+  version = 'dev',
+  description = 'Multi-protocoles Device Manager',
+  author = 'Pixel Stereo',
+  url='https://github.com/PixelStereo/pydevicemanager', 
+  download_url = 'https://github.com/PixelStereo/pydevicemanager/archive/master.zip', 
+  classifiers = [
+    'Development Status :: 2 - Pre-Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.4',
+    'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
